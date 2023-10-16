e05658fdb9334493333c2a50e852bc3eaa6bdd97
Keerthihub <hellokeerthivasan@gmail.com>
2023-10-16 20:41:22 +0530

    Make mobile navigation work JS

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEL = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function() {
+    headerEL.classList.toggle('nav-open');
+})
